5fa1620b34194a1daba0f1a7eb90006a25dda743
Shun-Hsiang Hu <shhu@gmail.com>
2015-03-20 13:09:17 +0800

    new file:   buildsInALAN.py         buildings are in a Class C subnet

---
+++ buildsInALAN.py
@@ -0,0 +1,67 @@
+from mininet.cli import CLI
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost, Controller, RemoteController
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+
+# Level: building, floor and room
+# There are mash between buildings
+# each building is tree topology
+
+class CampusTopo(Topo):
+    def __init__(self, building=2, floor=2, room=2, host=2, **opts):
+        Topo.__init__(self, **opts)
+        #create root switch of building
+        for b in range(building):
+            switch = self.addSwitch( 's%s' %(b+1) )
+
+        # create switches for each floor
+        for b in range(building):
+            for f in range(floor):
+                rootswitch = ('s%s' %(b+1))
+                switch = self.addSwitch( 's%s%s' %(b+1,f+1) )
+                self.addLink(switch, rootswitch)
+
+        for b in range(building):
+                for f in range(floor):
+                    for r in range(room):
+                        floorswitch = ( 's%s%s' %(b+1,f+1))
+                        switch = self.addSwitch( 's%s%s%s' %(b+1,f+1,r+1) )
+                        self.addLink(switch, floorswitch)
+
+        for b in range(building):
+            for f in range(floor):
+                for r in range(room):
+                    for h in range(host):
+                        roomswitch = ( 's%s%s%s' %(b+1,f+1,r+1))
+                        computer = self.addHost('h%s%s%s%s' % (b+1,f+1,r+1,h+1))
+                        self.addLink(computer, roomswitch)
+
+### simple link between root switch ###
+#       for b in range(1,building):
+#           switch1 = ( 's%s' %(b))
+#           switch2 = ( 's%s' %(b+1))
+#           self.addLink(switch1, switch2)
+
+#### mash ####
+        for b in range(1,building):
+            for x in range(b+1,building+1):
+                switch1 = ( 's%s' %(b))
+                switch2 = ( 's%s' %(x))
+                self.addLink(switch1, switch2)
+
+def simpleTest():
+    topo = CampusTopo(building=3,floor=3,room=2,host=3)
+    net = Mininet(topo, controller=RemoteController( 'c0', ip='127.0.0.1' ))
+    net.start()
+    print("Dumping host connections")
+    dumpNodeConnections(net.hosts)
+    print("Testing network connectivity")
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    simpleTest()
